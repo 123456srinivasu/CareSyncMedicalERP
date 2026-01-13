54c2b4467f8decd7c36aa59071b601af5d5014e7
Guna <guna.kkit@gmail.com>
2026-01-13 07:28:30 -0500

    update chief complaint
    update chief complaint

--- src/main/java/com/medical/caresync/repository/PatientChiefComplaintRepository.java
+++ src/main/java/com/medical/caresync/repository/PatientChiefComplaintRepository.java
@@ -4,7 +4,14 @@ import com.medical.caresync.entities.PatientChiefComplaint;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface PatientChiefComplaintRepository
         extends JpaRepository<PatientChiefComplaint, Long> {
+    Optional<PatientChiefComplaint>
+    findTopByPatientConsultation_PatientConsultationIdOrderByCreationTsDesc(
+            Long patientConsultationId
+    );
 }
